a16aefd55c8d5cf7f515e7d7304b3bba7e866dd0
Wouter <decosterwouter@gmail.com>
2017-11-18 19:02:18 +0100

    adding query_altmetric to class methods

--- promisingpreprint/preprint.py
+++ promisingpreprint/preprint.py
@@ -4,6 +4,7 @@
 from os import path
 import pickle
 from time import sleep
+from altmetric import Altmetric
 
 
 class Preprint(object):
@@ -23,6 +24,30 @@ class Preprint(object):
 
     def dry_print(self):
         print("{}\n{}".format(shorten(self.title), self.url))
+
+    def query_altmetric(self):
+        """Check the altmetric journal percentile score of the publication."""
+        a = Altmetric()
+        sleep(2)
+        try:
+            resp = a.doi(self.doi)
+            if resp:
+                pct = resp["context"]['journal']['pct']
+                assert 0 <= pct <= 100
+                return 0, pct
+            else:
+                return 0, 0
+        except Altmetric.HTTPException as e:
+            if e.status_code == 403:
+                return 403, "You aren't authorized for this call"
+            elif e.status_code == 420:
+                sleep(60)
+                return 420, "You are being rate limited"
+            elif e.status_code == 502:
+                return 502, "Down for maintenance."
+            elif e.status_code == 404:
+                return 404, "Invalid API function"
+                print(e.msg)
 
 
 def shorten(s):
