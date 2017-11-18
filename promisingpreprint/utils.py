15fa0ee42d8dfe2772934396e69a9a0eb3b8671b
Wouter <decosterwouter@gmail.com>
2017-11-18 18:36:55 +0100

    using a default logfile if file does not exist

--- promisingpreprint/utils.py
+++ promisingpreprint/utils.py
@@ -3,6 +3,7 @@
 
 import logging.handlers
 import logging
+import os
 
 
 def setup_logging(path):
@@ -10,9 +11,13 @@ def setup_logging(path):
     Setup a rotating log in which each file can maximally get 10kb
     5 backups are kept in addition to the current file
     '''
+    if os.path.isfile(path):
+        logfile = path
+    else:
+        logfile = "temporary_log.log"
     my_logger = logging.getLogger('MyLogger')
     my_logger.setLevel(logging.INFO)
-    handler = logging.handlers.RotatingFileHandler(path, maxBytes=10000, backupCount=5)
+    handler = logging.handlers.RotatingFileHandler(logfile, maxBytes=10000, backupCount=5)
     handler.setFormatter(logging.Formatter("{asctime} {levelname:8s} {message}", style='{'))
     my_logger.addHandler(handler)
     my_logger.info('Started.')
