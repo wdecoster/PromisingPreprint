761d0479e9a08e38370de3ba69b12249ce00dce8
Wouter <decosterwouter@gmail.com>
2017-11-18 18:25:00 +0100

    moving setup_logging to separate file

---
+++ promisingpreprint/utils.py
@@ -0,0 +1,19 @@
+# Part of PromisingPreprint
+# wdecoster
+
+import logging.handlers
+import logging
+
+
+def setup_logging(path):
+    '''
+    Setup a rotating log in which each file can maximally get 10kb
+    5 backups are kept in addition to the current file
+    '''
+    my_logger = logging.getLogger('MyLogger')
+    my_logger.setLevel(logging.INFO)
+    handler = logging.handlers.RotatingFileHandler(path, maxBytes=10000, backupCount=5)
+    handler.setFormatter(logging.Formatter("{asctime} {levelname:8s} {message}", style='{'))
+    my_logger.addHandler(handler)
+    my_logger.info('Started.')
+    return my_logger
