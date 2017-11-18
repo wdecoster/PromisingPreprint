6d4f1cd2119ebc93d3be8d04ab49eedb9155a947
Wouter <decosterwouter@gmail.com>
2017-11-18 17:54:04 +0100

    rewrite to functional programming, save to pickle

--- promisingpreprint/getPreprintsAndSave.py
+++ promisingpreprint/getPreprintsAndSave.py
@@ -6,50 +6,57 @@ import feedparser
 from os import path
 import logging
 import logging.handlers
+import pickle
 
 
-def checkRSS(dois_seen, dbf):
+class Preprint(object):
+    def __init__(self, doi, url, title, date):
+        self.doi = doi
+        self.url = url
+        self.title = title
+        self.date = date
+
+
+def checkRSS(preprints):
     '''
     Check the RSS feed of biorxiv and for each paper, save doi, link and title
     Except if the doi was already seen, then don't duplicate
     '''
+    dois_seen = [p.doi for p in preprints]
     feed = feedparser.parse("http://connect.biorxiv.org/biorxiv_xml.php?subject=all")
     if 'bozo_exception' in feed.keys():
+        pass
         my_logger.error("Failed to reach the feed")
     else:
         for pub in feed["items"]:
             if not pub['dc_identifier'] in dois_seen:
                 my_logger.info("Adding article {} to database".format(pub['dc_identifier']))
-                save2db(pub['dc_identifier'], trimlink(pub["link"]), pub["title"], pub['updated'], dbf)
+                preprints.append(
+                    Preprint(doi=pub['dc_identifier'],
+                             url=trimlink(pub["link"]),
+                             title=pub["title"],
+                             date=pub['updated']))
+        pickle.dump(
+            obj=preprints,
+            file=open(DATABASE, 'wb'))
+
 
 def trimlink(url):
-    '''
-    Remove unnessecary parts from the url
-    '''
+    """Remove unnessecary parts from the url."""
     return url.split('?')[0]
 
 
-def save2db(doi, link, title, date, dbf):
-    '''
-    Save article metrics to the database
-    Add status 'notTweeted', since this publication is new
-    '''
-    with open(dbf, 'a') as db:
-        db.write("{}\t{}\t{}\t{}\t{}\n".format(doi, link, title, date, 'notTweeted'))
-
-
-def readdb(dbf):
-    '''
-    Return all DOIs in the database file
-    '''
-    if path.isfile(dbf):
-        with open(dbf, 'r') as db:
-            return [i.split('\t')[0] for i in db]
+def load_database():
+    """If database exists, return content."""
+    if path.isfile(DATABASE):
+        with open(DATABASE, 'rb') as db:
+            return pickle.load(db)
     else:
         my_logger.warning("Unexpected: Database not found")
         return []
 
-def setupLogging():
+
+def setup_logging():
     '''
     Setup a rotating log in which each file can maximally get 10kb
     5 backups are kept in addition to the current file
@@ -66,15 +73,15 @@ def setupLogging():
 
 def main():
     try:
-        db = "/home/pi/projects/PromisingPreprint/preprintdatabase.txt"
-        dois_seen = readdb(db)
-        checkRSS(dois_seen, db)
-        my_logger.info('Finished.\n')
+        db = load_database()
+        checkRSS(db)
+        # my_logger.info('Finished.\n')
     except Exception as e:
         my_logger.error(e, exc_info=True)
         raise
 
 
 if __name__ == '__main__':
-    my_logger = setupLogging()
+    DATABASE = "/home/pi/projects/PromisingPreprint/preprintdatabase.pickle"
+    my_logger = setup_logging()
     main()
